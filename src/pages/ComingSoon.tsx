42660617c4fee848ccff82107e59f8205a803dde
Dan Fincher <danbfjr@gmail.com>
2022-01-31 20:31:12 -0500

    Updated coming soon page

--- src/pages/ComingSoon.tsx
+++ src/pages/ComingSoon.tsx
@@ -10,26 +10,12 @@ export const ComingSoon = (props: ComingSoonProps) => {
       width: "100vw",
       height: "100vh",
     }}>
-      <div style={{
-        backgroundColor: "#AE4A25",
-        height: "80%",
-        display: "flex",
-        flexDirection: "column",
-        // justifyContent: "space-around",
-        alignItems: 'center'
-      }}>     
-        <img src={logo} alt="Wedding Logo" width={500}/>
+      <div className="comingsoon-header">     
+        <img src={logo} alt="Wedding Logo" width={700}/>
         <span className="comingsoon-text light-text">are getting married</span>
         <span className="comingsoon-date">11.5.22</span>
       </div>
-      <div style={{
-        backgroundColor: "#E6C2A9",
-        height: "20%",  
-        display: "flex", 
-        flexDirection: "column",
-        justifyContent: "space-around",
-        alignItems: 'center'
-      }}>
+      <div className="comingsoon-footer">
         <span className="comingsoon-text dark-text">save the date, and check back often for more details</span>
       </div>
     </div>
