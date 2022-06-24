dbf9cc237733595871f64b8c34a417c0a4de1def
Dan Fincher <danbfjr@gmail.com>
2022-06-24 12:11:58 -0400

    Testing

--- src/pages/Test/Test.tsx
+++ src/pages/Test/Test.tsx
@@ -1,9 +1,22 @@
 import React from 'react';
+import axios from "axios";
 
 export const Test = () => {
+  const buttonClicked = async () => {
+    console.log("Reading from API...");
+    try {
+      const response = await axios.get("http://localhost:3001/api/gallery");
+      console.log("response: ", response);
+    } catch (error) {
+      console.log("ERROR: ", error);
+    }
+  }
   return (
     <div>
       Test Component
+      <div onClick={buttonClicked}>
+        button
+      </div>
     </div>
   )
 };
