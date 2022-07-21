33976f951ce10ba0b80391499b0f0e1bf1abfaba
Dan Fincher <danbfjr@gmail.com>
2022-07-21 17:18:49 -0400

    RSVP WIP

--- src/pages/RSVP/RSVP.tsx
+++ src/pages/RSVP/RSVP.tsx
@@ -5,160 +5,45 @@ import { NavBar } from "../../components/NavBar/NavBar";
 import { useMediaQuery } from 'react-responsive';
 import LogoSrc from "../../images/WeddingLogo.png";
 import { Card } from "../../components/Card/Card";
-import { Button } from "../../components/Button/Button";
-import { findRSVP } from "../../services/RSVP.service";
-import { Loader } from "../../components/Loader/Loader";
+import { RSVPLookup } from '../../components/RSVP/RSVPLookup';
+import { RSVPData } from '../../services/RSVP.service';
+import { RSVPDetail } from "../../components/RSVP/RSVPDetail";
 
 type RSVPProps = {
 
 };
 
-const ERROR_MSG: any = {
-  UNKNOWN: "Oops! Something went wrong. Try again.",
-  NOT_FOUND: "Your reservation was not found. Please make sure you entered your full name correctly.",
-  TOO_MANY_ATTEMPTS: "It seems you are still having trouble finding your reservation. Please reach out to Dan or Chandler.",
-  ALREADY_RSVP: "You have already RSVPed. If this was a mistake, please reach out to Dan or Chandler."
-};
-
-const ERROR_MAPPING: any = {
-  100: ERROR_MSG.UNKNOWN, // bad request
-  101: ERROR_MSG.UNKNOWN, // invalid parameter
-  102: ERROR_MSG.NOT_FOUND, 
-  103: ERROR_MSG.ALREADY_RSVP,
-};
-
 export const RSVP = (props: RSVPProps) => {
   const isMobile = useMediaQuery({ maxWidth: 767 });
-  const [ isFindReservationDisabled, setIsFindReservationDisabled ] = React.useState(true);
-  const [ reservationInputText, setReservationInputText ] = React.useState("");
-  const [ isLoading, setIsLoading ] = React.useState(false);
-  const [ errorMessage, setErrorMessage ] = React.useState("");
-  const [ attempts, setAttempts ] = React.useState(0);
-  const abortRef = React.useRef<AbortController | null>(null);
+  
+  const [ isLookupVisible, setIsLookupVisible ] = React.useState(true);
+  const [ isDetailVisible, setIsDetailVisible ] = React.useState(false);  
+  const [ rsvpData, setRsvpData ] = React.useState<RSVPData>();
 
-  const inputStyle: React.CSSProperties = {
-    height: isMobile ? "20px" : "40px",
-    borderRadius: isMobile ? "5px" : "10px",
-    backgroundColor: "#E4B2BC",
-    // color: "#E6C2A9",
-    fontFamily: "Curvilingus",
-    fontSize: isMobile ? "15px" : "30px",
-    width: isMobile ? "200px" : "400px",
-    marginTop: isMobile ? "20px" : "30px",
-    display: 'flex',
-    textAlign: 'center',
-    alignItems: "center",
-    justifyContent: "center",
-    borderColor: "transparent"
+  const RSVPOnSuccess = (rsvp: RSVPData) => {
+    console.log("RETRIEVED DATA: ", rsvp);
+    setRsvpData(rsvp);
+    setIsLookupVisible(false);
+    setIsDetailVisible(true);
   };
 
-  const findReservationContainerStyle: React.CSSProperties = {
-    // marginTop: isMobile ? "50px" : "100px",
-    // marginBottom: isMobile ? "150px" : "300px",
-  };
-
-  const findReservationStyle: React.CSSProperties = {
-    height: isMobile ? "40px" : "80px",
-    borderRadius: isMobile ? "5px" : "10px",
-    backgroundColor: "#CB683F",
-    color: "#E6C2A9",
-    fontFamily: "Curvilingus",
-    fontSize: isMobile ? "15px" : "30px",
-    width: isMobile ? "200px" : "400px",
-    margin: isMobile ? "10px" : "20px",
-    display: 'flex',
-    textAlign: 'center',
-    alignItems: "center",
-    justifyContent: "center",
-    cursor: "pointer",
-  };
-
-  const inputOnChange = (event: any) => {
-    const value = event.target.value;
-    setIsFindReservationDisabled(!value);
-    setReservationInputText(value);
-  };
-
-  const onKeyPress = (event: any) => {
-    if (reservationInputText && (event.key === "Enter" || event.keyCode === 13)) {
-      findReservation();
-    }
-  };
-
-  const findReservationClicked = () => {
-    findReservation();
-  };
-
-  const handleRSVPError = (errorCode: number): string => {
-    return ERROR_MAPPING[errorCode] ?? ERROR_MSG.UNKNOWN;
-  };
-
-  const findReservation = async () => {
-    console.log("finding reservation...");
-    setErrorMessage("");
-    setIsLoading(true);
-
-    if (abortRef.current) {
-      abortRef.current.abort();
-    }
-
-    try {
-      abortRef.current = new AbortController();
-      const response = await findRSVP(reservationInputText, abortRef.current.signal);
-      console.log("response: ", response);
-      const { data } = response;
-      if (data?.errorCode) {
-        abortRef.current = null;
-        setAttempts(attempts+1);
-        let errorMessage;
-        if (attempts >= 3) {
-          errorMessage = ERROR_MSG.TOO_MANY_ATTEMPTS;
-        } else {
-          errorMessage = handleRSVPError(data.errorCode);
-        }
-        setErrorMessage(errorMessage);
-      } else {  // success
-        setAttempts(0); // reset attempts
-
-        // show rsvp detail screen
-      }
-    } catch (error: any) {
-      abortRef.current = null;
-      setAttempts(attempts+1);
-      console.log("RSVP ERROR", JSON.stringify(error));
-      setErrorMessage(ERROR_MSG.UNKNOWN);
-    }
-    setIsLoading(false);
-  };
+  const onDetailBackButtonClicked = () => {
+    setIsLookupVisible(true);
+    setIsDetailVisible(false);
+  }; 
 
   return (
     <div className="container" style={{height: "100vh"}}>
       <NavBar/>
       <div className="rsvp-container" style={{ marginTop: isMobile ? "44px" : undefined}}> 
         <img className="logo-img" src={LogoSrc} alt="Wedding Logo" style={{marginTop: "50px", maxWidth: isMobile ? "50%" : undefined }}/>
-        <Card style={{marginTop: isMobile ? "25px" : "50px"}}>
+        <Card style={{
+            marginTop: isMobile ? "25px" : "50px", 
+            marginBottom: isMobile ? "300": "600px"
+          }}>
           <div className="inner-card">
-            <div className='rsvp-text' style={isMobile ? {fontSize: "15px", width: "80%"} : undefined}>
-              If you're responding for you and a guest (or your family), you'll be able to RSVP for your entire group.
-            </div>
-            <input 
-              placeholder='Full Name' 
-              style={inputStyle} 
-              onChange={inputOnChange}
-              onKeyPress={onKeyPress}
-            />
-            <Button 
-              disabled={isFindReservationDisabled}
-              title={`Find your reservation`} 
-              style={findReservationStyle} 
-              containerStyle={findReservationContainerStyle}
-              onClick={findReservationClicked}
-              onMouseEnter={(setStyleState) => {
-                setStyleState({...findReservationStyle, opacity: .5})
-              }}
-            />
-            { errorMessage && <div>{ errorMessage }</div>}
-            <Loader isLoading={isLoading}/>
+            { isLookupVisible && <RSVPLookup onSuccess={RSVPOnSuccess}/> }
+            { rsvpData && isDetailVisible && <RSVPDetail onBackButtonClicked={onDetailBackButtonClicked} rsvpData={rsvpData}/> }
           </div>
         </Card>
       </div>
--- src/services/RSVP.service.ts
+++ src/services/RSVP.service.ts
@@ -1,9 +1,20 @@
 import { mongoInstance } from "./Axios.service";
 
+export type RSVPData = {
+  alternateNames: string[],
+  name: string,
+  rsvpCount: number,
+  totalCount: number,
+  _id: string,
+  rsvp: {
+    [key: string]: boolean,
+  }
+};
+
 export const findRSVP = async (name: string, signal?: AbortSignal): Promise<any> => {
   return mongoInstance.get("/endpoint/findRSVP", { 
     signal,
-    params: { name }
+    params: { name: name.toLowerCase() }
   });
 };
 
