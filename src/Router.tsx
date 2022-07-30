a61c092311e2a57bca78e57ef5104cfb2d4d3806
Dan Fincher <danbfjr@gmail.com>
2022-07-30 14:30:53 -0400

    Removed Test

--- src/Router.tsx
+++ src/Router.tsx
@@ -10,7 +10,6 @@ import { Registry } from "./pages/Registry/Registry";
 import { Wedding } from './pages/Wedding/Wedding';
 import { Travel } from './pages/Travel/Travel';
 import { Gallery } from './pages/Gallery/Gallery';
-import { Test } from './pages/Test/Test';
 import { NavBar } from "./components/NavBar/NavBar";
 import { HamburgerMenu } from "./components/HamburgerMenu/HamburgerMenu";
 import ScrollToTop from './components/ScrollToTop/ScrollToTop';
@@ -34,7 +33,6 @@ export const Router = () => {
         <Route path="travel" element={<Travel />} />
         <Route path="registry" element={<Registry />} />
         <Route path="gallery" element={<Gallery />} />
-        <Route path="test" element={<Test />} />
       </Routes>
     </BrowserRouter>
   );
