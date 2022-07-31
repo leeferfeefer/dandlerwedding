54a24f1d3301257f6413194d6af851e61291da4b
Dan Fincher <danbfjr@gmail.com>
2022-07-31 13:06:36 -0400

    Updated server location

--- src/services/Axios.service.ts
+++ src/services/Axios.service.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const apiInstance = axios.create({
-  baseURL: 'http://localhost:3001/api/',
+  baseURL: 'https://dandlerwedding-api.herokuapp.com/api/',
   timeout: 10000,
 });
 
