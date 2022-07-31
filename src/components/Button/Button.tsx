fa037137de0097380b59000c81d4d96c8a65b291
Dan Fincher <danbfjr@gmail.com>
2022-07-31 11:13:23 -0400

    Removed comment from button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -13,36 +13,6 @@ type ButtonProps = {
   disabled?: boolean;
 };
 
-
-/**
- * 
- * @param props React.useEffect(() => {
-export const Button = (props: ButtonProps) => {
-  const { onClick, style, title, containerStyle, imgSrc,
-    componentRef, onMouseEnter, onMouseLeave, buttonClassName, disabled } = props;
-  const [ styleState, setStyleState ] = React.useState<React.CSSProperties>({});
-
-  React.useEffect(() => {
-    if (disabled) {
-      setStyleState({ ...style, backgroundColor: 'gray', cursor: 'not-allowed'});
-    } else {
-      setStyleState({ ...style, backgroundColor: style?.backgroundColor, cursor: style?.cursor});
-    }
-  }, [disabled, style]);
-  
-  const _onMouseEnter = () => {
-    !disabled && onMouseEnter && onMouseEnter(setStyleState);
-  };
-
-  const _onMouseLeave = () => {
-    if (!disabled) {
-      onMouseLeave && onMouseLeave();
-      setStyleState(style ?? {});
-    }
-  };
- * @returns 
- */
-
 const RealButton = (props: ButtonProps) => {
   const { onClick, style, title, imgSrc, componentRef, imgStyle,
     disabled } = props;
